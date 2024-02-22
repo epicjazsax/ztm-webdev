9b2d7bea116be714fe6d5c4db9a03ffbc8faf894
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-02-22 10:49:39 -0800

    Complete Keyless Car first draft

--- JSexercises/script.js
+++ JSexercises/script.js
@@ -29,4 +29,25 @@ function operate() {
 	alert(`${first} ${operator} ${second} = ${answer}`)
 }
 
-operate();
+function driverAge() {
+	var age = Number(prompt("enter your age so we can check your suitability"));
+	if (age <= 0) {
+		alert("THE UNBORN CANNOT DRIVE");
+	} else if (age >= 70) {
+		alert("SKELETONS CANNOT DRIVE");
+	} else {
+		if (age > 16) {
+			alert("Drive safe you little devil you");
+		} else if (age < 16) {
+			alert("CHILDREN CANNOT DRIVE");
+		} else if (age === 16) {
+			alert("Don't crash it'll stay on your insurance for way longer than you'd think necessary");
+		} else {
+			alert("MUST SUBMIT A VALID AGE");
+			setTimeout(driverAge, 1);
+			return;
+		}
+	}
+}
+
+driverAge()
