ecc07b5a9aeb1c2eb56b6bfa989d8b00be3b7d25
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-02-28 12:10:03 -0800

    Implement first draft of delete button via id#

--- DOMexercises/script.js
+++ DOMexercises/script.js
@@ -1,7 +1,7 @@
 const taskList = document.getElementById("taskList");
-var task = document.querySelectorAll("li");
 const taskInput = document.getElementById("newTaskInput");
 const taskSubmit = document.getElementById("newTaskSubmit");
+const trashCans = document.getElementById("trashCans");
 
 //take taskInput.value and add it to taskList ul, then clear taskInput box
 function newTask() {
@@ -9,6 +9,7 @@ function newTask() {
 		let task = document.createElement("li");
 		task.appendChild(document.createTextNode(taskInput.value));
 		taskList.appendChild(task);
+		//clear input box
 		taskInput.value = "";
 	}
 }
@@ -23,7 +24,22 @@ taskSubmit.addEventListener("click", newTask);
 
 //click to add "done" strike-through class to task
 taskList.addEventListener("click", (event) => {
-	const targetId = event.target.getAttribute("id");
-	const currentTask = document.getElementById(targetId);
-	currentTask.classList.toggle("done");
+	let targetId = event.target.getAttribute("id");
+	if (targetId != "taskList") {
+		let currentTask = document.getElementById(targetId);
+		currentTask.classList.toggle("done");
+	}
 });
+
+//click trashcan icon to delete its related task
+trashCans.addEventListener("click", (event) => {
+	let trashId = event.target.getAttribute("id");
+	if (trashId != "trashCans") {
+		let trashIcon = document.getElementById(trashId);
+		let taskId = "task" + trashId.slice(4);
+		let task = document.getElementById(taskId);
+		trashIcon.remove();
+		task.remove();
+		console.log(`${taskId} removed`);
+	}
+})
