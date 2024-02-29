80dbc95b7daee15b3f9adccd5b2ab8aa87006ad8
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-02-29 11:56:22 -0800

    Complete "add new task" first draft

--- DOMexercises/script.js
+++ DOMexercises/script.js
@@ -1,35 +1,52 @@
-const taskList = document.getElementById("taskList");
+const todoList = document.getElementById("todoList");
 const taskInput = document.getElementById("newTaskInput");
 const taskSubmit = document.getElementById("newTaskSubmit");
-const trashCans = document.getElementById("trashCans");
-
-// //take taskInput.value and add it to taskList ul, then clear taskInput box
-// function newTask() {
-// 	if (taskInput.value) {
-// 		let task = document.createElement("li");
-// 		task.appendChild(document.createTextNode(taskInput.value));
-// 		taskList.appendChild(task);
-// 		//clear input box
-// 		taskInput.value = "";
-// 	}
-// }
-// //make Enter key in taskInput box click submit button
-// taskInput.addEventListener("keydown", function(event) {
-// 	if (event.key ==="Enter") {
-// 		taskSubmit.click();
-// 	}
-// });
-// //make submit button add newTask to taskList
-// taskSubmit.addEventListener("click", newTask);
-
-//click to add "done" strike-through class to task
-document.querySelectorAll("tr td:nth-child(even)")
-.forEach(e => e.addEventListener("click", function() {
-	e.classList.toggle("done");
-}));
 
 //function to delete row, called by trash can in HTML
 function deleteRow(r) {
   var i = r.parentNode.parentNode.rowIndex;
-  document.getElementById("todoList").deleteRow(i);
+  todoList.deleteRow(i);
 }
+
+//function to add taskInput.value to todoList table, then clear taskInput box
+function newTask() {
+	if (taskInput.value) {
+		let row = todoList.insertRow();
+		let cell1 = row.insertCell(0);
+		let cell2 = row.insertCell(1);
+
+		//set up cell1 (delete button cell)
+		let trashCell = document.createElement("input");
+		trashCell.type = "button";
+		trashCell.value = "🗑";
+		trashCell.setAttribute("onclick", "deleteRow(this)");
+		cell1.appendChild(trashCell);
+
+		//set up cell2 (task cell)
+		let taskCell = document.createElement("div");
+		taskCell.appendChild(document.createTextNode(taskInput.value));
+		cell2.appendChild(taskCell);
+		cell2.addEventListener("click", function() {
+			cell2.classList.toggle("done");
+		});
+
+		//clear taskInput box
+		taskInput.value = "";
+	}
+}
+
+//make submit button run newTask function to add taskInput to todoList
+taskSubmit.addEventListener("click", newTask);
+
+//make Enter key in taskInput box click submit button
+taskInput.addEventListener("keydown", function(event) {
+	if (event.key ==="Enter") {
+		taskSubmit.click();
+	}
+});
+
+//click to add "done" strike-through class to task
+document.querySelectorAll("table td")
+.forEach(e => e.addEventListener("click", function() {
+	e.classList.toggle("done");
+}));
