affa49c63455714321f3a7700cc54c3ea4edbc68
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-03-18 15:20:09 -0700

    Update to support any and all matches in array

--- AdvJS/additionexercise.js
+++ AdvJS/additionexercise.js
@@ -1,36 +1,54 @@
 //write function to return an array of which two numbers of given array add up to given integer
-const doesNumAddIntoInt = (num, arr, int) => {
+const numPlusArrayElementEqualsSum = (num, arr, sum) => {
 	for (element of arr) {
-		if (element + num === int) {
-			return [element, num]
+		if (num + element === sum) {
+			return [num, element]
 		}
 	};
 }
 
-//currently doesn't work if arr[0] is not one of the numbers that adds to int
-const whichTwoAdd = (arr, int) => {
-	for (num of arr) {
-		let currentElementTest = doesNumAddIntoInt(num, arr, int);
-		console.log(num, arr, int, currentElementTest);
-		if (currentElementTest) {
-			return currentElementTest, "if return"
-		}
+//will return duplicates of answer-arrays if the original given array contains multiple of the same integer that belongs in an answer-array
+//ex. given array [1,1,2] and target sum of 3, return of allArraysThatAddToSum will be [[1,2],[1,2]] (one match for each 1)
+//is this even a problem we should solve?
+//idea: eliminate duplicates from given array at start?
+const whichTwoAdd = (arr, sum) => {
+	let remainingArray = [];
+	let allArraysThatAddToSum = [];
+	remainingArray = remainingArray.concat(arr);
+	console.log("array: ", arr);
+	console.log("sum integer: ", sum);
+	console.log("initial 'remaining array': ", remainingArray);
+	console.log("initial 'allArrays': ", allArraysThatAddToSum);
+	while (remainingArray.length > 1) {
+		let num = remainingArray.shift();
+		let elementsThatAddToSum = numPlusArrayElementEqualsSum(num, remainingArray, sum);
+		console.log("current element: ", num);
+		console.log("'remaining array': ", remainingArray);
+		console.log("array of current element and remainingArray element that add to sum: ", elementsThatAddToSum);
+		if (typeof elementsThatAddToSum === "object") {
+			allArraysThatAddToSum = allArraysThatAddToSum.concat([elementsThatAddToSum]);
+			console.log("allArrays updated to: ", allArraysThatAddToSum)
+		};
 	};
+	return allArraysThatAddToSum
 };
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 //test input variables
 const givenArr = [1,2,3];
-const givenInt = 4;
+const givenSum = 4;
 const secondArr = [3,5,2];
-const secondInt = 7;
-const simpleArr = [5,6,7];
-const simpleInt = 11;
+const secondSum = 7;
+const doubleArr = [7,2,1,3,4];
+const doubleSum = 5;
+const duplicateArr = [6,1,2,1];
+const duplicateSum = 3;
 
 //test output variables
-const givenGoal = [1,3];
-const secondGoal = [5,2];
-const simpleGoal = [5,6];
+const givenGoal = [[1,3]];
+const secondGoal = [[5,2]];
+const doubleGoal = [[2,3],[1,4]];
+const duplicateGoal = [[1,2]];
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 //test
@@ -39,42 +57,45 @@ const simpleGoal = [5,6];
 	//orders both arrays to make comparing simpler
 	//checks that both ordered arrays are of same length and the value at each index is the same for both
 
+
+//UPDATE VERIFIER FUNCTIONS TO ACCOUNT FOR NESTED ARRAYS
 const sameArrayVerifier = (arr1, arr2) => (arr1.length === arr2.length) && arr1.every(function(e, i) {return e === arr2[i]})
 
-const whichTwoFunctioningProperly = (arr, int, goal) => {
-	const whichTwoOutputSorted = whichTwoAdd(arr, int).toSorted((a,b) => a-b);
+const whichTwoFunctioningProperly = (arr, sum, goal) => {
+	const whichTwoOutputSorted = whichTwoAdd(arr, sum).toSorted((a,b) => a-b);
 	const goalSorted = goal.toSorted((a,b) => a-b);
 	return sameArrayVerifier(whichTwoOutputSorted, goalSorted)
 };
 
 //unit tests
-const givenElementTest = doesNumAddIntoInt(1, givenArr, givenInt);
-console.log("numIntoInt function returns array from Given values: ", typeof givenElementTest === "object");
-console.log("numIntoInt function return from Given values: ", givenElementTest);
-console.log("numIntoInt return adds up to given int from Given values: ", givenElementTest[0] + givenElementTest[1] === givenInt);
+const testNumIntoInt = (num, arr, sum, name) => {
+	const elementTest = numPlusArrayElementEqualsSum(num, arr, sum);
+	console.log(name, "numIntoInt function returns object: ", typeof elementTest === "object");
+	console.log(name, "numIntoInt function returns: ", elementTest);
+	if (elementTest) {
+		console.log(name, "numIntoInt return adds up to given sum from Given values: ", elementTest[0] + elementTest[1] === sum);
+	};
+};
 
-const givenAddTest = whichTwoAdd(givenArr, givenInt);
-console.log("whichTwoAdd function returns array from Given values: ", typeof givenAddTest === "object");
-console.log("whichTwoAdd function return from Given values: ", givenAddTest);
-console.log("whichTwoAdd return adds up to given int from Given values: ", givenAddTest[0] + givenAddTest[1] === givenInt);
+const testWhichTwoAdd = (arr, sum, name) => {
+	const addTest = whichTwoAdd(arr, sum);
+	console.log(name, "whichTwoAdd function returns object: ", typeof addTest === "object");
+	console.log(name, "whichTwoAdd function returns: ", addTest);
+	if (addTest) {
+		//UPDATE TO ACCOUNT FOR NESTED ARRAYS
+		console.log(name, "whichTwoAdd return adds up to given sum: ", addTest[0] + addTest[1] === sum);
+	};
+};
 
-const secondElementTest = doesNumAddIntoInt(3, secondArr, secondInt);
-console.log("numIntoInt function returns array from Second values: ", typeof secondElementTest === "object");
-console.log("numIntoInt function return from Second values: ", secondElementTest);
-console.log("numIntoInt return adds up to given int from Second values: ", secondElementTest[0] + secondElementTest[1] === secondInt);
-
-const secondAddTest = whichTwoAdd(secondArr, secondInt);
-console.log("whichTwoAdd function returns array from Second values: ", typeof secondAddTest === "object");
-console.log("whichTwoAdd function return from Second values: ", secondAddTest);
-console.log("whichTwoAdd return adds up to given int from Second values: ", secondAddTest[0] + secondAddTest[1] === secondInt);
+testWhichTwoAdd(givenArr, givenSum, "given");
+testWhichTwoAdd(secondArr, secondSum, "second");
+testWhichTwoAdd(doubleArr, doubleSum, "double");
+testWhichTwoAdd(duplicateArr, duplicateSum, "duplicate");
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 //acceptance tests-ish
-console.log("data type of return value of whichTwoAdd for given values is object: ", (typeof whichTwoAdd(givenArr, givenInt) === "object"));
-console.log("given variables have passed: ", whichTwoFunctioningProperly(givenArr, givenInt, givenGoal));
+console.log("given variables have passed: ", whichTwoFunctioningProperly(givenArr, givenSum, givenGoal));
 
-console.log("data type of return value of whichTwoAdd for second values is object: ", (typeof whichTwoAdd(secondArr, secondInt) === "object"));
-console.log("second variables have passed: ", whichTwoFunctioningProperly(secondArr, secondInt, secondGoal));
+console.log("second variables have passed: ", whichTwoFunctioningProperly(secondArr, secondSum, secondGoal));
 
-console.log("data type of return value of whichTwoAdd for simple values is object: ", (typeof whichTwoAdd(simpleArr, simpleInt) === "object"));
-console.log("simple variables have passed: ", whichTwoFunctioningProperly(simpleArr, simpleInt, simpleGoal));
+console.log("simple variables have passed: ", whichTwoFunctioningProperly(simpleArr, simpleSum, simpleGoal));
