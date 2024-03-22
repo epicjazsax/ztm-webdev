4dbd26354667b9d047136724c379a6a1d5940162
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-03-21 17:51:32 -0700

    Update main translator to use switch/case

--- AdvJS/colorcodetranslator.js
+++ AdvJS/colorcodetranslator.js
@@ -12,8 +12,16 @@ const rgb3 = "rgb(16, 157, 180)";
 //-----------------------------------------------------------------------------------------
 //note for rgbToHex: use integerVariableName.toString(16) to turn r, g, then b to hex
 
-//check that argument given is a valid HEX color code
+//check that argument given is a valid HEX or RGB color code
 const isItAHexColorCode = (colorCode) => (typeof colorCode === "string" && colorCode.length === 7 && colorCode[0] === "#");
+const isItAnRGBColorCode = (colorCode) => (typeof colorCode === "string" && colorCode.slice(0,4) === "rgb(" && colorCode.slice(-1) === ")");
+const isItHexOrRGB = (colorCode) => {
+	if (isItAHexColorCode(colorCode)) {
+		return "HEX"
+	} else if (isItAnRGBColorCode(colorCode)) {
+		return "RGB"
+	};
+};
 
 //converts HEX color code to rgb
 const convertHexToRGB = (hex) => {
@@ -22,12 +30,18 @@ const convertHexToRGB = (hex) => {
 	return `rgb(${red}, ${green}, ${blue})`
 };
 
-//potentially add a switch for hex/rgb/default instead of if/else
+//converts RGB color code to HEX
+const convertRGBToHex = (rgb) => {};
+
+//takes given HEX or RGB color code and translates it to the other type of color code
 const translateColorCode = (code) => {
-	if (isItAHexColorCode(code)) {
-		return convertHexToRGB(code)
-	} else {
-		console.log("PLEASE INPUT A HEX COLOR CODE WITH FORMAT '#123456'");
+	switch (isItHexOrRGB(code)) {
+		case "HEX":
+			return convertHexToRGB(code)
+		case "RGB":
+			return convertRGBToHex(code)
+		default:
+			return "please input a valid HEX code (ex. #123456) or RGB code (ex. rgb(1,10,100) )"
 	};
 };
 
@@ -37,8 +51,20 @@ const logIsTestVariableHexCode = (testVariable, name) => {
 	console.log(`${name}: ${testVariable} is a HEX color code: `, isItAHexColorCode(testVariable));
 };
 
+const logIsTestVariableRGBCode = (testVariable, name) => {
+	console.log(`${name}: ${testVariable} is an RGB color code: `, isItAnRGBColorCode(testVariable));
+};
+
+const testIsItHexOrRGB = (testVariable, name) => {
+	console.log(`${name}: ${testVariable} is HEX or RGB color code: `, isItHexOrRGB(testVariable));
+};
+
 const testConvertHexToRGB = (hex, rgb, name) => {
 	console.log(`${name}: ${hex} has converted to ${rgb} properly with HexToRGB: `, convertHexToRGB(hex) === rgb);
+};
+
+const testConvertRGBToHex = (rgb, hex, name) => {
+	console.log(`${name}: ${rgb} has converted to ${hex} properly with RGBToHex: `, convertRGBToHex(rgb) === hex);
 };
 
 const testTranslateColorCode = (inputCode, expectation, name) => {
@@ -47,18 +73,46 @@ const testTranslateColorCode = (inputCode, expectation, name) => {
 
 //-----------------------------------------------------------------------------------------
 //run tests
-
+console.log("Run all Test Variables to see if they are HEX codes");
 logIsTestVariableHexCode(hex1, "hex1");
 logIsTestVariableHexCode(rgb1, "rgb1");
 logIsTestVariableHexCode(hex2, "hex2");
 logIsTestVariableHexCode(rgb2, "rgb2");
 logIsTestVariableHexCode(hex3, "hex3");
 logIsTestVariableHexCode(rgb3, "rgb3");
-
+console.log("-----------------------------------------------------------------------------");
+console.log("Run all Test Variables to see if they are RGB codes");
+logIsTestVariableRGBCode(hex1, "hex1");
+logIsTestVariableRGBCode(rgb1, "rgb1");
+logIsTestVariableRGBCode(hex2, "hex2");
+logIsTestVariableRGBCode(rgb2, "rgb2");
+logIsTestVariableRGBCode(hex3, "hex3");
+logIsTestVariableRGBCode(rgb3, "rgb3");
+console.log("-----------------------------------------------------------------------------");
+console.log("Run all Test Variables and some false values through HEX or RGB function");
+testIsItHexOrRGB(hex1, "hex1");
+testIsItHexOrRGB(rgb1, "rgb1");
+testIsItHexOrRGB(hex2, "hex2");
+testIsItHexOrRGB(rgb2, "rgb2");
+testIsItHexOrRGB(hex3, "hex3");
+testIsItHexOrRGB(rgb3, "rgb3");
+testIsItHexOrRGB(1234, "1234");
+testIsItHexOrRGB("hello", "hello");
+console.log("-----------------------------------------------------------------------------");
+console.log("Run all HEX variables through ConvertHexToRGB");
 testConvertHexToRGB(hex1, rgb1, "hex1");
 testConvertHexToRGB(hex2, rgb2, "hex2");
 testConvertHexToRGB(hex3, rgb3, "hex3");
-
+console.log("-----------------------------------------------------------------------------");
+console.log("Run all RGB Variables through ConvertRGBToHex");
+testConvertRGBToHex(rgb1, hex1, "rgb1");
+testConvertRGBToHex(rgb2, hex2, "rgb2");
+testConvertRGBToHex(rgb3, hex3, "rgb3");
+console.log("-----------------------------------------------------------------------------");
+console.log("run all Test Variables through TranslateColorCode");
 testTranslateColorCode(hex1, rgb1, "hex1");
 testTranslateColorCode(hex2, rgb2, "hex2");
 testTranslateColorCode(hex3, rgb3, "hex3");
+testTranslateColorCode(rgb1, hex1, "rgb1");
+testTranslateColorCode(rgb2, hex2, "rgb2");
+testTranslateColorCode(rgb3, hex3, "rgb3");
