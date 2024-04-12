ce58486cb1018bff992642fcd3ff6b57f87366e5
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-04-12 13:16:29 -0700

    Add tests for Card
    This was a lot of experimentation to get JS/React tests working.
    
    See also:
      - https://jestjs.io/docs/api
      - https://testing-library.com/docs/react-testing-library/api
      - https://testing-library.com/docs/queries/bytext

--- robofriends/src/components/Card.test.js
+++ robofriends/src/components/Card.test.js
@@ -1,29 +1,47 @@
-import { queryAllByText, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Card from './Card';
 
-// describe('empty card', () => {
-// 	const emptyCard = Card();
+describe('simple card', () => {
+	it('contains cardName, cardUsername and cardEmail elements', () => {
+		const { getByText, queryAllByText } = render(
+			<Card name='person1' username='user1' email='user1@foo.com' />
+	  	);
+		expect(getByText('person1')).toHaveProperty('className', 'cardName');
+		expect(getByText('@user1')).toHaveProperty('className', 'cardUsername');
+		expect(getByText('user1@foo.com')).toHaveProperty('className', 'cardEmail');
+		expect(queryAllByText(/./)).toHaveLength(3);
+	})
+})
 
-// 	it('contains a Name element', () => {
-// 		expect(emptyCard).toHaveClass('cardName')
+describe('partially constructed card', () => {
+	it('initializes name, but not username or email', () => {
+		const { getByText, queryByText, queryAllByText } = render(
+			<Card name='person1'/>
+	  	);
+		expect(getByText('person1')).toHaveProperty('className', 'cardName');
+		// Card should have one text field if only one is initialized
+		expect(queryAllByText(/./)).toHaveLength(1);
+	})
+})
+
+describe('test by role', () => {
+	it('basic', () => {
+		const { getAllByRole } = render(
+			<Card name='person1' username='user1' email='user1@foo.com' />
+	  	);
+		// Card should have 3 text fields if fully initialized
+		expect(getAllByRole('heading', {level: 2})).toHaveLength(1);
+		// expect(getAllByRole('paragraph')).toHaveLength(1);
+	})
+})
+
+// // Uncomment below to show debug printout of rendered output
+// describe('experiment with debug', () => {
+// 	it('basic', () => {
+// 		const { debug } = render(
+// 			<Card name='person1' username='user1' email='user1@foo.com' />
+// 	  	);
+// 		debug();
 // 	})
 // })
 
-// describe('Nea Karlsson card', () => {
-// 	const nea = {
-// 		id: 1,
-// 		name: 'Nea Karlsson',
-// 		username: 'Mashtyx',
-// 		email: 'urbanevader@dbd.bhvr',
-// 	};
-// 	const neaCard = Card(nea);
-// 	const cardName = neaCard.getByText(nea.name)
-
-// 	it('contains a Name element', () => {
-// 		expect(cardName).toBe(nea.name)
-// 	})
-
-	// it('displays Neas name', () => {
-	// 	expect(neaCard).toHaveTextContent(nea.name)
-	// })
-})
