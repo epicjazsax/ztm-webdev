7b573c854138b26cb7b7c49c2b2a966235955e93
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-04-26 11:54:46 -0700

    Update main App to use Hooks

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import { robots as besties } from '../robots';
@@ -7,49 +7,40 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import 'tachyons';
 import './App.css';
 
-class App extends Component {
-	constructor () {
-		super()
-		this.state = {
-			robots: besties,
-			searchfield: '',
-		}
-	}
+function App() {
+	const [robots, setRobots] = useState(besties);
+	const [searchfield, setSearchfield] = useState('');
 
-	componentDidMount() {
+	useEffect(() => {
 		fetch('https://jsonplaceholder.cypress.io/users')
 			.then(res => res.json())
 			.then(data => {
-				this.setState({ robots: besties.concat(data) })
+				setRobots(besties.concat(data))
 			})
-	}
+	}, [])
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value)
 	}
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
 
-	render () {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		return !robots.length ?
-			<div id="Loading">Loading...</div> :
-			(
-				<div className='tc container'>
-					<div id='header'>
-						<h1 className='f1'>HI BESTIES</h1>
-						<SearchBox searchChange={this.onSearchChange}/>
-					</div>
-					<Scroll id='scroll'>
-						<ErrorBoundary>
-							<CardList robots={filteredRobots}/>
-						</ErrorBoundary>
-					</Scroll>
+	return !robots.length ?
+		<div id="Loading">Loading...</div> :
+		(
+			<div className='tc container'>
+				<div id='header'>
+					<h1 className='f1'>HI BESTIES</h1>
+					<SearchBox searchChange={onSearchChange}/>
 				</div>
-			);
-
-	}
+				<Scroll id='scroll'>
+					<ErrorBoundary>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundary>
+				</Scroll>
+			</div>
+		);
 }
 
 export default App
